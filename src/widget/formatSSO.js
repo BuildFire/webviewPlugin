cbef7ed70467cbd51f3438c664fd50d807df6563
attila buturla <abuturla@buildfire.com>
2018-04-04 15:35:28 -0700

    Minor refactor

--- src/widget/formatSSO.js
+++ src/widget/formatSSO.js
@@ -1,11 +1,14 @@
 export const formatSSO = (url, ssoUserString) => {
+  let userToken = '';
+
   if (ssoUserString) {
     const ssoUserObj = JSON.parse(ssoUserString);
     const tokenExists = ssoUserObj.ssoUserToken;
-    return url.replace('{{SSO}}', tokenExists
-      ? decodeURIComponent(ssoUserObj.ssoUserToken)
-      : '');
+
+    userToken  =  (tokenExists)
+        ? decodeURIComponent(ssoUserObj.ssoUserToken)
+        : '';
   }
 
-  return url.replace('{{SSO}}', '');
+  return url.replace('{{SSO}}', userToken);
 };
